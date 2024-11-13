eb1f267ba56b00f5029ba8a8e402b01b07baad13
Tino Schr <tino.schreiber@t-systems.com>
2024-11-13 15:00:25 +0100

    Update tox.ini && conf.py file (#13)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: SebastianGode

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -107,12 +107,7 @@ templates_path = ['_templates']
 html_copy_source = False
 
 # -- Options for PDF output --------------------------------------------------
-latex_documents = [
-    ('index',
-     'None.tex',
-     u'Config - User Guide',
-     u'OpenTelekomCloud', 'manual'),
-]
+latex_documents = []
 
 # Get the Git commit values for last updated timestamp on each page
 repo = Repo(search_parent_directories=True)
@@ -124,6 +119,11 @@ latex_elements = {
   'papersize': 'a4paper',
   'pointsize': '12pt',
   'figure_align': 'H',
-  'preamble': r'\newcommand{\githash}{' + current_commit_hash + '}',
+  'preamble': rf'''
+        \newcommand{{\githash}}{{{current_commit_hash}}}
+        \newcommand{{\gitcommittime}}{{{current_commit_time}}}
+        \newcommand{{\doctitle}}{{{otcdocs_doc_title}}}
+        \newcommand{{\servicetitle}}{{{otcdocs_service_title}}}
+  ''',
   'sphinxsetup': 'hmargin={15mm,15mm}, vmargin={20mm,30mm}, marginpar=10mm'
 }
